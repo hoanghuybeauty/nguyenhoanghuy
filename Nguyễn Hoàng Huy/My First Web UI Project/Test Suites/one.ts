9f26a4c477882258b6d8e43638dd29f8bbd954ba
hoanghuybeauty@gmail.com <66257807+hoanghuybeauty@users.noreply.github.com>
2020-09-26 20:19:40 +0700

    huy đăng lên

---
+++ Nguyễn Hoàng Huy/My First Web UI Project/Test Suites/one.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>one</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>ca3d0be1-0684-4866-acfd-23620a178019</testSuiteGuid>
+   <testCaseLink>
+      <guid>c40b2406-4b16-4f5b-baf5-a14589752185</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/01</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9305c172-7cff-4810-92b4-983f5f504a82</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/02</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>72894d96-2d47-4aba-95f3-c777a0e6b7ea</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/03</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6f166758-be59-4931-a879-be9953bdfd78</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/04</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>210c75f1-94b2-430d-a606-2b4b7bf613fa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/05</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
